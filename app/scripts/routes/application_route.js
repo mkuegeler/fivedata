d43eefd87b5c50c00fe79062c5310da207fc37df
mkuegeler <cc@kuegeler.com>
2014-02-03 01:00:37 +0100

    initial commit
    includes grunt, bower , yo and ember templates

---
+++ app/scripts/routes/application_route.js
@@ -0,0 +1,7 @@
+Fivedata.ApplicationRoute = Ember.Route.extend({
+    // admittedly, this should be in IndexRoute and not in the
+    // top level ApplicationRoute; we're in transition... :-)
+    model: function () {
+        return ['black', 'yellow', 'blue'];
+    }
+});
---
+++ app/scripts/store.js
@@ -0,0 +1,1 @@
+Fivedata.ApplicationAdapter = DS.FixtureAdapter;
