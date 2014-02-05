9f0af479ce2c55a73053e18fa67226c5f7abcf11
mkuegeler <cc@kuegeler.com>
2014-02-05 21:08:21 +0100

    sample.svg added

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,6 @@ Fivedata.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['black', 'yellow', 'blue'];
+        return ['node 1', 'node 2', 'node 3'];
     }
 });
